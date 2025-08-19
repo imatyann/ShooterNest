2c4332b9332c660df9a912b423a698d7002c8bc2
今泉隼人 <imatyann2003@gmail.com>
2025-08-19 18:15:18 +0900

    add all selected

--- game/piece.py
+++ game/piece.py
@@ -5,11 +5,12 @@ from . import settings
 
 class Piece:
 
-    def __init__(self, color, current, radius, width_color):
+    def __init__(self, color, current, radius, width_color, selected_color):
         self.color = color
         self.current = current
         self.radius = radius
         self.width_color = width_color
+        self.selected_color = selected_color
     
     def center_pos(self,origin, board_square_size):
         return (
@@ -17,17 +18,24 @@ class Piece:
             origin[1] + self.current[1] * board_square_size + board_square_size/2,
             )
 
-    def draw(self, screen, board_square_size, origin):
+    def draw(self, screen, board_square_size, origin, selected_piece):
+        if selected_piece != self:
+            current_width_color = self.color
+            current_radius = self.radius
+        else:
+            current_width_color = self.selected_color
+            current_radius = self.radius - 1
+
         pygame.draw.circle(screen, 
-                           self.color, 
+                           current_width_color, 
                            self.center_pos(origin,board_square_size), 
-                           self.radius,
+                           current_radius,
                            )
         pygame.draw.circle(screen, 
-                           self.width_color, 
-                           self.center_pos(origin,board_square_size),
-                           self.radius,
-                           width = 1)
+                        self.width_color, 
+                        self.center_pos(origin,board_square_size),
+                        current_radius,
+                        width = 1)
         
     def move(self, target):
         self.current = target
@@ -36,4 +44,10 @@ class Piece:
         if math.hypot(pos[0] - self.center_pos(origin, board_square_size)[0], pos[1] - self.center_pos(origin, board_square_size)[1]) < self.radius:        
             return True
         else:
-            return False        
+            return False
+    
+    # def clicked(self,selected_piece):
+    #     if selected_piece == self:
+    #         self.is_selected = True
+    #     else:
+    #         self.is_selected = False
--- main.py
+++ main.py
@@ -22,13 +22,15 @@ def start():
         settings.BOARD_ORIGIN,
         settings.CELL_SIZE 
     )
+    selected_piece = None
     
     # 赤駒初期化
     red_piece = piece.Piece(
         settings.RED_COLOR,
         settings.RED_START_POSITION,
         settings.PIECE_RADIUS, 
-        settings.WIDTH_COLOR
+        settings.WIDTH_COLOR,
+        settings.RED_SELECTED_COLOR
     )
 
     # 青駒初期化
@@ -36,7 +38,8 @@ def start():
         settings.BLUE_COLOR,
         settings.BLUE_START_POSITION,
         settings.PIECE_RADIUS, 
-        settings.WIDTH_COLOR
+        settings.WIDTH_COLOR,
+        settings.BLUE_SELECTED_COLOR
     )
 
     # 緑駒初期化
@@ -44,9 +47,16 @@ def start():
         settings.GREEN_COLOR,
         settings.GREEN_START_POSITION,
         settings.PIECE_RADIUS, 
-        settings.WIDTH_COLOR
+        settings.WIDTH_COLOR,
+        settings.GREEN_SELECTED_COLOR
     )
 
+    # 盤面の占有情報
+    occupied = {}
+    occupied[red_piece.current] = red_piece
+    occupied[blue_piece.current] = blue_piece
+    occupied[green_piece.current] = green_piece
+    
     # 毎秒実行する関数
     running = True
     while running:
@@ -59,14 +69,24 @@ def start():
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 pos_mouse = event.pos
                 if red_piece.tatch(pos_mouse,main_board.origin, main_board.size):
-                    red_piece.move((2,2))
+                    selected_piece = red_piece
+                elif blue_piece.tatch(pos_mouse,main_board.origin, main_board.size):
+                    selected_piece = blue_piece
+                elif green_piece.tatch(pos_mouse,main_board.origin, main_board.size):
+                    selected_piece = green_piece
+                else:
+                    selected_piece = None
 
         # オブジェクトの描画
         screen.fill(settings.BG_COLOR)
+        
         main_board.draw(screen)
-        red_piece.draw(screen, main_board.size,main_board.origin)
-        blue_piece.draw(screen, main_board.size,main_board.origin)
-        green_piece.draw(screen, main_board.size,main_board.origin)
+        
+        red_piece.draw(screen, main_board.size,main_board.origin,selected_piece)
+
+        blue_piece.draw(screen, main_board.size,main_board.origin,selected_piece)
+
+        green_piece.draw(screen, main_board.size,main_board.origin,selected_piece)
 
         # 画面更新
         pygame.display.flip()
